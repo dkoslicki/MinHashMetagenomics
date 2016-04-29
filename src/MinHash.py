664dc19cf92299c75c9df679c33bd332edde8e70
dkoslicki <dmkoslicki@gmail.com>
2016-04-28 21:06:14 -0700

    chunk size 700

--- src/MinHash.py
+++ src/MinHash.py
@@ -250,7 +250,7 @@ def import_multiple_hdf5(input_files_list):
     #for file_name in input_files_list:
     #    CEs.append(import_single_hdf5(file_name))
     pool = Pool(processes=multiprocessing.cpu_count())
-    CEs = pool.map_async(import_single_hdf5, input_files_list,chunksize=50).get()
+    CEs = pool.map_async(import_single_hdf5, input_files_list, chunksize=700).get()
     pool.close()
 
     return CEs
