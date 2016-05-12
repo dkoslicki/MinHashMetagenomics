20ded859851dca54e5dd95853a08597771abf386
dkoslicki <dmkoslicki@gmail.com>
2016-05-12 23:28:33 +0100

    raise exception if command too long

--- src/MinHash.py
+++ src/MinHash.py
@@ -28,9 +28,9 @@ warnings.simplefilter("ignore", RuntimeWarning)
 # To Do:
 # Implement hash_murmur3 to leave the khmer package. Need to implement reverse complement myself, etc.
 # After that point, can use amino acids
-# Get SNAP implemented
 # SNAP paired or single reads
 # Get DIAMOND implemented
+# Make the snap streaming automatically chunk the index_dirs if there are too many
 
 
 notACTG = re.compile('[^ACTG]')
@@ -962,8 +962,11 @@ def stream_aligned_save_unaligned(index_dirs, sample_file, out_file, filt='unali
                 raise Exception("aligned must be 'aligned', 'unaligned', or 'all'")
             big_cmd = big_cmd + " | " + cmd
         i += 1
-    print(big_cmd)
-    #exit_code = subprocess.call(big_cmd, shell=True,  stdout=FNULL, stderr=subprocess.STDOUT)
+    if len(big_cmd) >= 2616670:
+        raise Exception("The typical maximum command length is 2616670, and running it with this many indicies would exceed that. Please iterate over index_dirs in chunks.")
+    else:
+        exit_code = subprocess.call(big_cmd, shell=True,  stdout=FNULL, stderr=subprocess.STDOUT)
+    return exit_code
 
 
 def sam2fastq(sam_file, out_file):
