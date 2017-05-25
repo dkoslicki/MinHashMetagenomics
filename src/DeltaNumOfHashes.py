114662fe95430ffe8e48d317718141cacc7edd5c
hoomanzabeti <hoomanzabeti@gmail.com>
2017-05-24 18:06:32 -0700

    Added Jac estimation to delta-k plot

--- src/DeltaNumOfHashes.py
+++ src/DeltaNumOfHashes.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def deltaHashNumPlot(cardAIntB, cardAUB, cont, conf, p, m, deltaStep, deltabeginpnt):
-    delta= np.arange(deltabeginpnt, 1., deltaStep)
-    kj= (-3 * np.log(conf/2) * cardAUB) / (np.sqrt(delta)*cardAIntB)
-    kc = (-3 * (cont+p) * np.log(conf/2) * np.sqrt(cardAUB+(1+delta)*cardAIntB)) / (np.sqrt(cont*delta*cardAUB))
-    return delta,kj,kc
+    delta = np.arange(deltabeginpnt, 1., deltaStep)
+    kj = (-3 * np.log(conf/2) * cardAUB) / (np.sqrt(delta)*cardAIntB)
+    kc = (-3*(cont+p)*np.log(conf/2))/np.sqrt(cont*delta)
+    k_est = (-3 * (cont+p) * np.log(conf/2) * np.sqrt(cardAUB+(1+delta)*cardAIntB)) / (np.sqrt(cont*delta*cardAUB))
+    return delta, kj, kc, k_est
